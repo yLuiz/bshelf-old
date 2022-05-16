9d74473a05eb59937daf8f1cf9c3a21ca316b2f6
Luiz Victor <luizvictor1231@gmail.com>
2022-05-16 14:23:13 -0400

    Deploy with dist

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const { resolve } = require('path')
+const app = express()
+
+app.use('/', express.static(
+
+  resolve(
+    __dirname,
+    './dist'
+  )
+  ))
+
+app.listen(process.env.PORT || 3000)
